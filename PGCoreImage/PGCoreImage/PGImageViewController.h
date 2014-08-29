15f9ef72e279ee8a08a693cfe8f094695438fb69
PC Nguyen <pnguyen@reputation.com>
2014-08-29 15:52:57 -0700

    pod integration

---
+++ PGCoreImage/PGCoreImage/PGImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PGImageViewController.h
+//  PGCoreImage
+//
+//  Created by PC Nguyen on 8/29/14.
+//  Copyright (c) 2014 PC Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGImageViewController : UIViewController
+
+@end
